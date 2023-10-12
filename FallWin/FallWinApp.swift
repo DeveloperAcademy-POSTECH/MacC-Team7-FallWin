ea2c36c0284a3d98073629f32b093de231425273
soc06212 <soc06212@gmail.com>
2023-10-12 17:32:15 +0900

    Initial Commit

---
+++ FallWin/FallWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FallWinApp.swift
+//  FallWin
+//
+//  Created by 최명근 on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FallWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
