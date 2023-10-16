8e2c1940cff14cd85dad9ce0bfa8b5fad8138d8c
soc06212 <soc06212@gmail.com>
2023-10-17 00:49:06 +0900

    [Feat] TabView에 포함되는 네 개의 View (Gallery, Search, Surf, Profile) 및 Feature 개발 [#3]

--- FallWin/View/ContentView.swift
+++ FallWin/View/ContentView.swift
@@ -6,19 +6,45 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ContentView: View {
+    let store: StoreOf<Feature>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            CvasTabView(selection: viewStore.binding(get: \.tabSelection, send: Feature.Action.tabSelect)) {
+                
+                IfLetStore(store.scope(state: \.$gallery, action: Feature.Action.gallery)) { store in
+                    GalleryView(store: store)
+                        .tabItem(.init(title: "Gallery", image: "circle.fill", tabItem: .gallery), selection: viewStore.binding(get: \.tabSelection, send: Feature.Action.tabSelect))
+                }
+                
+                IfLetStore(store.scope(state: \.$search, action: Feature.Action.search)) { store in
+                    SearchView(store: store)
+                        .tabItem(.init(title: "Search", image: "circle.fill", tabItem: .search), selection: viewStore.binding(get: \.tabSelection, send: Feature.Action.tabSelect))
+                }
+                
+                IfLetStore(store.scope(state: \.$surf, action: Feature.Action.surf)) { store in
+                    SurfView(store: store)
+                        .tabItem(.init(title: "Surf", image: "circle.fill", tabItem: .surf), selection: viewStore.binding(get: \.tabSelection, send: Feature.Action.tabSelect))
+                }
+                
+                IfLetStore(store.scope(state: \.$profile, action: Feature.Action.profile)) { store in
+                    ProfileView(store: store)
+                        .tabItem(.init(title: "Profile", image: "circle.fill", tabItem: .profile), selection: viewStore.binding(get: \.tabSelection, send: Feature.Action.tabSelect))
+                }
+                
+            }
+            .onAppear {
+                viewStore.send(.initViews)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(store: Store(initialState: Feature.State(), reducer: {
+        Feature()
+    }))
 }
