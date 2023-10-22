ad98f9d09a0b55bb66093936a0dcc7774a5f69ca
hyeok3013 <kdh301397@gmail.com>
2023-10-22 21:15:53 +0900

    [Feat][SearchView] 전반적인 디자인 수정 및 실제데이터 클릭시 상세뷰 넘어가는 거 구현

--- FallWin/View/Search/SearchView.swift
+++ FallWin/View/Search/SearchView.swift
@@ -20,24 +20,35 @@ struct SearchView: View {
                 ScrollView() {
                     if viewStore.searchTerm.isEmpty {
                         LazyVGrid(columns: columns, spacing: 4) {
-                            ForEach(viewStore.groupedSearchResults.sorted(by: { $0.key > $1.key }), id: \.key) { key, searchResults in
+                            ForEach(viewStore.groupedSearchResults.sorted(by: { $0.key < $1.key }), id: \.key) { key, searchResults in
                                 if !searchResults.isEmpty {
                                     Section(header: HStack {
                                         Text(key)
+                                            .font(Font.custom("Pretendard", size: 20).weight(.semibold))
                                         Spacer()
                                     }
-                                                
-                                        .font(Font.custom("Pretendard", size: 20).weight(.semibold))
-                                    ) {
+                                        .padding(.vertical, 12)
+                                        .padding(.top, 12)
+                                        
+                                    ){
                                         ForEach(searchResults, id: \.self) { journal in
-                                            Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))
-                                                .cornerRadius(4)
-                                                .frame(width: 115, height: 115)
-                                                .padding(4)
-                                                
-                                                .onTapGesture {
-                                                    viewStore.send(.showJournalView(journal))
+                                            ZStack {
+                                                if let image = journal.wrappedImage {
+                                                    Image(uiImage: image)
+                                                        .cornerRadius(4)
+                                                        .frame(width: 115, height: 115)
+                                                        .padding(4)
+                                                } else {
+                                                    Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))
+                                                        .cornerRadius(4)
+                                                        .frame(width: 110, height: 110)
+                                                        .padding(.vertical, 1.5)
+                                                        
                                                 }
+                                            }
+                                            .onTapGesture {
+                                                viewStore.send(.showJournalView(journal))
+                                            }
                                         }
                                     }
                                 }
@@ -45,22 +56,33 @@ struct SearchView: View {
                         }
                     }else {
                         LazyVGrid(columns: columns, spacing: 4) {
-                           
-                            ForEach((viewStore.searchResults), id: \.self) { _ in
-                                    Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))
+                            
+                            ForEach((viewStore.searchResults), id: \.self) { journal in
+                                if let image = journal.wrappedImage {
+                                    Image(uiImage: image)
                                         .cornerRadius(4)
                                         .frame(width: 115, height: 115)
                                         .padding(4)
+                                } else {
                                     
                                 }
+                                
+                            }
                         }
                     }
                 }
                 .fullScreenCover(store: store.scope(state: \.$journal, action: SearchFeature.Action.journal)) { store in
                     JournalView(store: store)
                 }
+                .navigationTitle(
+                    Text("검색")
+                )
+                .padding(.horizontal, 12)
+                .padding(.top, -12)
                 
-                #if targetEnvironment(simulator)
+                
+                
+#if targetEnvironment(simulator)
                 HStack{
                     Button(action: {
                         let context = PersistenceController.shared.container.viewContext
@@ -71,7 +93,8 @@ struct SearchView: View {
                         j1.image = nil
                         j1.mind = 1
                         j1.timestamp = Date()
-                        context.insert(j1)
+//                        j1.timestamp = Calendar.current.date(byAdding: .month, value: -1, to: Date())
+//                        context.insert(j1)
                         do {
                             try context.save() // 변경 내용 저장
                             print("Data saved")
@@ -87,46 +110,46 @@ struct SearchView: View {
                     Button(action: {
                         
                         deleteData()
-      
+                        
                     }, label: {
                         Text("더미데이터 삭제")
                         
                     }).padding(.bottom, 63)
                 }
                 //TODO: 테스트 이후 삭제
-                #endif
+#endif
             }
-            
             .onAppear {
                 viewStore.send(.fetchData)
             }
             .onChange(of: viewStore.searchTerm) { newValue in
-                                viewStore.send(.filterData(newValue))
+                viewStore.send(.filterData(newValue))
             }
             .padding(.horizontal, 12)
-            .navigationTitle("검색")
+            
             .searchable(text: viewStore.binding(get: { $0.searchTerm }, send: { .setSearchTerm($0) }), placement: .navigationBarDrawer(displayMode: .always), prompt: Text("찾고 싶은 추억을 입력해보세요"))
         }
+        
     }
     
     // 삭제할 데이터를 검색
     func deleteData() {
         let context = PersistenceController.shared.container.viewContext
         let fetchRequest: NSFetchRequest<Journal> = Journal.fetchRequest()
-
+        
         do {
             let results = try context.fetch(fetchRequest)
             for journal in results {
                 context.delete(journal) // 데이터 삭제
             }
-
+            
             try context.save() // 변경 내용 저장
             print("Data deleted")
         } catch {
             print("Error deleting data: \(error)")
         }
     }
-
+    
 }
 
 // TODO: 테스트 이후에 더미데이터에 관련한 기능들 삭제
@@ -137,4 +160,5 @@ struct SearchView: View {
     SearchView(store: Store(initialState: SearchFeature.State(), reducer: {
         SearchFeature()
     }))
+    
 }
