5966ac63fc7f80338014867288aba0556f35237f
hyeok3013 <kdh301397@gmail.com>
2023-11-10 16:43:54 +0900

    백업 뷰 디자인 수정

--- FallWin/View/Settings/BackupSettingView.swift
+++ FallWin/View/Settings/BackupSettingView.swift
@@ -15,22 +15,18 @@ struct BackupSettingView: View {
     
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
-            VStack(alignment: .center){
-//                HStack{
-//                    Spacer()
-//                }
+            VStack(alignment: .center, spacing: 9){
+
                 
                 VStack(alignment: .leading){
-                    Text("- 오화는 사용자가 작성한 일기 데이터를 저장하지 않아요.")
+                    Text("- 픽다는 사용자가 작성한 일기 데이터를 저장하지 않아요.")
                         .font(.pretendard(.medium, size: 16))
                         .foregroundStyle(.textTeritary)
                     Text("- 데이터를 안전하게 보관하려면 iCloud에 주기적으로 백업해주세요.")
                         .font(.pretendard(.medium, size: 16))
                         .foregroundStyle(.textTeritary)
                 }
-                
-                
-                
+                .padding(.top, 24)
                   
                 Spacer()
                 
@@ -46,9 +42,16 @@ struct BackupSettingView: View {
                     Text("iCloud 백업")
                         .font(.pretendard(.semiBold, size: 18))
                         .foregroundStyle(.textOnButton)
-                        .frame(width: 350, height: 50)
+                        .frame(maxWidth: .infinity, minHeight: 45)
                 })
                 .buttonStyle(.borderedProminent)
+                .alert(isPresented: $isclickedBackup, content: {
+                    Alert(title: Text("iCloud 백업")
+                        .font(.pretendard(.bold, size: 20))
+                          , message: Text("iCloud 백업이 완료되었습니다.")
+                        .font(.pretendard(.medium, size: 16))
+                          , dismissButton: .default(Text("완료")))
+                })
                 
                 
                 Button(action: {
@@ -57,19 +60,26 @@ struct BackupSettingView: View {
                     Text("iCloud 복원")
                         .font(.pretendard(.semiBold, size: 18))
                         .foregroundStyle(.textOnButton)
-                        .frame(width: 350, height: 50)
+                        .frame(maxWidth: .infinity, minHeight: 45)
                 })
                 .buttonStyle(.borderedProminent)
+                .alert(isPresented: $isclickedRestore, content: {
+                    Alert(title: Text("iCloud 복원")
+                        .font(.pretendard(.bold, size: 20))
+                          , message: Text("iCloud 복원이 완료되었습니다.")
+                        .font(.pretendard(.medium, size: 16))
+                          , dismissButton: .default(Text("완료")))
+                })
                   
             }
+            
+            .frame(maxWidth: .infinity)
             .padding([.leading, .trailing], 20)
             .padding(.bottom, 15)
             
-            .alert(isPresented: $isclickedRestore, content: {
-                Alert(title: Text("iCloud 복원"), message: Text("iCloud 복원이 완료되었습니다."), dismissButton: .default(Text("확인")))
-            })
+            
             .padding(.top, 32)
-            .padding(.horizontal, 20)
+            .padding(.horizontal, 15)
             
               
         }
